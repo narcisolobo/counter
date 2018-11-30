05e45e5bcc980d07c8429c925172430b1b434c1e
Narciso Lobo <44825108+narcisolobo@users.noreply.github.com>
2018-11-30 16:37:19 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -5,15 +5,27 @@ app.secret_key = 'on top of spaghetti'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    session['counter'] = 1
+    return render_template('index.html', counter=session['counter'])
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    session['counter'] = 1
+    return render_template('index.html', counter=session['counter'])
 
 @app.route('/counter_post', methods=['POST'])
 def counter_post():
+    session['counter'] += 1
+    return redirect('/show_counter')
+
+@app.route('/counter_post2', methods=['POST'])
+def counter_post2():
+    session['counter'] += 2
     return redirect('/show_counter')
 
 @app.route('/show_counter')
 def show_counter():
-    return render_template('show.html')
+    return render_template('show.html', counter=session['counter'])
 
 if __name__ == '__main__':
     app.run(debug=True)
